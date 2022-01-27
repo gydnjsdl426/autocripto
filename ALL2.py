79a0e8562a1af1136d87c3a484154f8d9fc8c9ba
gydnjsdl426 <51254788+gydnjsdl426@users.noreply.github.com>
2022-01-27 16:56:11 +0900

    Update ALL2.py

--- ALL2.py
+++ ALL2.py
@@ -74,7 +74,7 @@ def startGamble(name):
         for n in name:
             krw = upbit.get_balance("KRW")
             if upbit.get_balance(n) == 0:
-                if current_price[n]*1.018 < predicted_max_price[i] and get_ma15(n) and krw > 5000:
+                if current_price[n]*1.013 < predicted_max_price[i] and get_ma15(n) and krw > 5000:
                     upbit.buy_market_order(n, total*0.33)
         
             elif ((upbit.get_avg_buy_price(n) * 1.01 > current_price[n] and current_price[n]*0.997 > predicted_min_price[i])
