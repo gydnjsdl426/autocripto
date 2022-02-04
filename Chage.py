c112d2d937cf7847c8131fa75aafe6d440bb1d87
gydnjsdl426 <51254788+gydnjsdl426@users.noreply.github.com>
2022-02-04 18:16:29 +0900

    Update Chage.py

--- Chage.py
+++ Chage.py
@@ -28,6 +28,24 @@ def get_total():
         total = float(total) + float(balance['balance']) * float(balance['avg_buy_price'])
     return total
 
+def update_target():
+    tickers = pyupbit.get_tickers(fiat="KRW")
+    all = pyupbit.get_current_price(tickers)
+    i=0
+    target_prices=[]
+    for ticker in tickers:
+        try:
+            target_prices.insert(i,get_target_price(ticker, 1.2))
+            time.sleep(0.1)
+            i += 1
+        except Exception as e:
+            print(e)
+            time.sleep(1)
+
+    return target_prices
+
+target_prices=update_target()
+
 # 자동매매 시작
 while True:
     try:
@@ -39,20 +57,23 @@ while True:
         all = pyupbit.get_current_price(tickers)
         total = get_total()
         krw = upbit.get_balance("KRW") * 0.097
+        i=0
         if start_time < now < end_time - datetime.timedelta(seconds=60):
             for ticker in tickers:
-                target_price = get_target_price(ticker, 0.9)
-                time.sleep(0.12)
-                if target_price < all[ticker] and target_price > all[ticker]*1.03 and krw > 5000 and upbit.get_balance(ticker) == 0:
-                    upbit.buy_market_order(ticker, total*0.097)
+                if target_prices[i] < all[ticker] and target_prices[i] * 1.02 > all[ticker] and krw > 5000 and upbit.get_balance(ticker) == 0:
+                    upbit.buy_market_order(ticker, total*0.197)
                 
-                elif upbit.get_avg_buy_price(ticker) * 1.4 > all[ticker] and upbit.get_balance(ticker) != 0:
+                elif upbit.get_avg_buy_price(ticker) * 1.3 < all[ticker] and upbit.get_balance(ticker) != 0:
                     upbit.sell_market_order(ticker, upbit.get_balance(ticker))
+
+                i+=1
 
         else:
             for ticker in tickers:
                 if upbit.get_balance(ticker) != 0:
                     upbit.sell_market_order(ticker, upbit.get_balance(ticker))
+            
+            target_prices=update_target()
 
         time.sleep(1)
     except Exception as e:
