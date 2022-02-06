02a4182393f0e18c822c36d5b8171958c2391f53
gydnjsdl426 <51254788+gydnjsdl426@users.noreply.github.com>
2022-02-07 03:38:45 +0900

    Update Chage.py

--- Chage.py
+++ Chage.py
@@ -30,36 +30,42 @@ def get_total():
         total = float(total) + float(balance['balance']) * float(balance['avg_buy_price'])
     return total
 
-def get_ror(ticker ,k=0.5):
+def get_ror(ticker):
     df = pyupbit.get_ohlcv(ticker, interval = "day", count=7)
-    time.sleep(0.07)
-    df['range'] = (df['high'] - df['low']) * k
-    df['target'] = df['open'] + df['range'].shift(1)
+    ror=[]
+    map={'ticker':'name','index':0,'value':0}
+    for k in np.arange(0, 1.0, 0.1):
+        df['range'] = (df['high'] - df['low']) * k
+        df['target'] = df['open'] + df['range'].shift(1)
 
-    fee = 0.0032
-    df['ror'] = np.where(df['high'] > df['target'],
-                         df['close'] / df['target'] - fee,
-                         1)
+        fee = 0.0032
+        df['ror'] = np.where(df['high'] > df['target'],
+                            df['close'] / df['target'] - fee,
+                            1)
 
-    ror = df['ror'].cumprod()[-2]
-    return ror
+        ror.append(df['ror'].cumprod()[-2])
+
+    map['ticker']=ticker
+    map['index']=ror.index(max(ror)) / 10
+    map['value']=max(ror)
+    return map
 
 def update_target():
     tickers = pyupbit.get_tickers(fiat="KRW")
-    i=0
+ 
     target_prices=[]
+    maps=[]
     for ticker in tickers:
         try:
-            ror=[]
-            for k in np.arange(0, 1.0, 0.1):
-                ror.append(get_ror(ticker, k))
-                
-            best_ror=ror.index(max(ror)) / 10
-            target_prices.insert(i, get_target_price(ticker, best_ror+0.4))
-            i += 1
+            maps.append(get_ror(ticker))
+            time.sleep(0.1)
         except Exception as e:
             print(e)
             time.sleep(0.7)
+
+    data = sorted(maps,key= lambda v:(v['value']),reverse=True)
+    for i in np.arange(0,15):
+        target_prices.append({'ticker':data[i]['ticker'],'price':get_target_price(data[i]['ticker'],data[i]['index'])})
 
     return target_prices
 
@@ -72,14 +78,17 @@ while True:
         start_time = get_start_time("KRW-BTC")
         end_time = start_time + datetime.timedelta(days=1)
 
-        tickers = pyupbit.get_tickers(fiat="KRW")
+        tickers = []
+        for i in np.arange(0,15):
+            tickers.append(target_prices[i]['ticker'])
+
         all = pyupbit.get_current_price(tickers)
         total = get_total()
-        krw = upbit.get_balance("KRW") * 0.097
+        krw = upbit.get_balance("KRW")
         i=0
         if start_time < now < end_time - datetime.timedelta(seconds=180):
             for ticker in tickers:
-                if target_prices[i] < all[ticker] and target_prices[i] * 1.1 > all[ticker] and krw > 5000 and upbit.get_balance(ticker) == 0:
+                if target_prices[i]['price'] <= all[ticker] and target_prices[i]['price'] * 1.1 >= all[ticker] and krw > 5000 and upbit.get_balance(ticker) == 0:
                     upbit.buy_market_order(ticker, total*0.097)
 
                 elif upbit.get_avg_buy_price(ticker) * 1.3 < all[ticker] and upbit.get_balance(ticker) != 0:
