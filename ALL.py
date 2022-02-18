815dd68b40f5eb990df1fe711cd302322b01aacb
gydnjsdl426 <51254788+gydnjsdl426@users.noreply.github.com>
2022-02-18 19:50:07 +0900

    Update ALL.py

--- ALL.py
+++ ALL.py
@@ -1,106 +1,73 @@
 import time
 import pyupbit
 import datetime
+import numpy as np
 import schedule
-from fbprophet import Prophet
 
 access = "KHTqX6NbkdKaY7saJo40FCjRF8zhLvB2WkADnajD"
 secret = "vIIe18R6kquGERglLWfKSC5UxKxfQA6DP4rq2UWc"
 
-def get_target_price(ticker, k):
-    """변동성 돌파 전략으로 매수 목표가 조회"""
-    df = pyupbit.get_ohlcv(ticker, interval="day", count=2)
-    target_price = df.iloc[0]['close'] + (df.iloc[0]['high'] - df.iloc[0]['low']) * k
-    return target_price
-
-def get_start_time(ticker):
-    """시작 시간 조회"""
-    df = pyupbit.get_ohlcv(ticker, interval="day", count=1)
-    start_time = df.index[0]
-    return start_time
-
-def get_balance(ticker):
-    """잔고 조회"""
-    balances = upbit.get_balances()
-    for b in balances:
-        if b['currency'] == ticker:
-            if b['balance'] is not None:
-                return float(b['balance'])
-            else:
-                return 0
-    return 0
-
-def get_current_price(ticker):
-    """현재가 조회"""
-    return pyupbit.get_orderbook(ticker=ticker)["orderbook_units"][0]["ask_price"]
-
-predicted_close_price = 0
-def predict_price(ticker):
-    """Prophet으로 당일 종가 가격 예측"""
-    global predicted_close_price
-    df = pyupbit.get_ohlcv(ticker, interval="second60")
-    df = df.reset_index()
-    df['ds'] = df['index']
-    df['y'] = df['close']
-    data = df[['ds','y']]
-    model = Prophet()
-    model.fit(data)
-    future = model.make_future_dataframe(periods=24, freq='H')
-    forecast = model.predict(future)
-    closeDf = forecast[forecast['ds'] == forecast.iloc[-1]['ds'].replace(hour=9)]
-    if len(closeDf) == 0:
-        closeDf = forecast[forecast['ds'] == data.iloc[-1]['ds'].replace(hour=9)]
-    closeValue = closeDf['yhat'].values[0]
-    predicted_close_price = closeValue
-predict_price("KRW-AQT")
-schedule.every().hour.do(lambda: predict_price("KRW-AQT"))
-
-def getTotal():
-    aqt = get_balance("AQT") * get_current_price("AQT")
-    matic = get_balance("MATIC") * get_current_price("MATIC")
-    eth = get_balance("ETH") * get_current_price("ETH")
-    powr = get_balance("POWR") * get_current_price("POWR")
-    stx = get_balance("STX") * get_current_price("STX")
-    xrp = get_balance("XRP") * get_current_price("XRP")
-    return get_balance("KRW")+aqt+matic+eth+powr+stx+xrp
-
-def startGamble(name):
-    try:
-        now = datetime.datetime.now()
-        start_time = get_start_time(name)
-        end_time = start_time + datetime.timedelta(days=1)
-        schedule.run_pending()
-        tick=name[4:]
-
-        if start_time < now < end_time - datetime.timedelta(seconds=10):
-            target_price = get_target_price(name, 0.2)
-            current_price = get_current_price(name)
-            krw = get_balance("KRW")
-            
-            total = getTotal()
-            if target_price < current_price and current_price < predicted_close_price:
-                
-                if krw > 5000 and get_balance(tick) == 0:
-                    upbit.buy_market_order(name, total*0.1665)
-        else:
-            
-            if get_balance(tick) > 0.00008:
-                upbit.sell_market_order(name, get_balance(tick))
-        time.sleep(1)
-    except Exception as e:
-        print(e)
-        time.sleep(1)
-
-# 로그인
 upbit = pyupbit.Upbit(access, secret)
+tickers = pyupbit.get_tickers(fiat="KRW")
 print("autotrade start")
 
-# 자동매매 시작
+def get_total():
+    balances = upbit.get_balances()
+    total = float(balances[0]['balance'])
+    for balance in balances:
+        total = float(total) + float(balance['balance']) * float(balance['avg_buy_price'])
+    return total
+
+data=[]
+TICKERS=[]
+def update():
+    for ticker in tickers:
+        df = pyupbit.get_ohlcv(ticker, interval = "day", count=3)
+        df['range'] = df['high'] / df['low']
+        d = df['range'].rolling(3).mean().iloc[-1]
+        if d > 1.095:
+            data.append(d)
+            TICKERS.append(ticker)
+
+        time.sleep(0.1)
+
+update()
+schedule.every(1).hours.do(update)
+
+mean20 = 0
+ma5= 0
+def get_ma(ticker):
+    df = pyupbit.get_ohlcv(ticker, interval="minute1", count=25)
+    sum=0
+    for i in np.arange(0,20):
+        sum+=df['close'][i:5+i].rolling(5).mean().iloc[-1]  
+
+    mean20=sum/20
+    ma5=df['close'].rolling(5).mean().iloc[-1]
+
+    print(ticker)
+    print(mean20)
+    print(ma5)
+
+    return mean20 < ma5 < mean20*0.01
+
 while True:
-    startGamble("KRW-MATIC")
-    startGamble("KRW-AQT")
-    startGamble("KRW-ETH")
-    startGamble("KRW-POWR")
-    startGamble("KRW-STX")
-    startGamble("KRW-XRP")
-    
+    schedule.run_pending()
+    try:
+        all = pyupbit.get_current_price(TICKERS)
+        total = get_total()
+        
+
+        for ticker in TICKERS:
+            krw = upbit.get_balance("KRW")
+            if get_ma(ticker) and all[ticker] > mean20 and upbit.get_balance(ticker) == 0:
+                if krw > 5000:
+                    upbit.buy_market_order(ticker, total*0.245)
+
+            elif upbit.get_balance(ticker) != 0 and not(upbit.get_avg_buy_price(ticker) * 0.995 < all[ticker] < upbit.get_avg_buy_price(ticker) * 1.007):
+                upbit.sell_market_order(ticker, upbit.get_balance(ticker))
+            time.sleep(0.08)
+            
+    except Exception as e:
+        print(e)
+        time.sleep(0.5)
