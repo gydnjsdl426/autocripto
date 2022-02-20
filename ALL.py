95727e2386b15427f43c9abc8a7ac2412cbf6fe2
gydnjsdl426 <51254788+gydnjsdl426@users.noreply.github.com>
2022-02-20 21:23:23 +0900

    Update ALL.py

--- ALL.py
+++ ALL.py
@@ -26,61 +26,62 @@ def get_total():
 data=[]
 TICKERS=[]
 def update():
+    for ticker in TICKERS:
+            if upbit.get_balance(ticker) != 0:
+                upbit.sell_market_order(ticker, upbit.get_balance(ticker))
+
     for ticker in tickers:
         df = pyupbit.get_ohlcv(ticker, interval = "day", count=3)
         df['range'] = df['high'] / df['low']
         d = df['range'].rolling(3).mean().iloc[-1]
-        if d > 1.1:
+        if d > 1.11:
             data.append(d)
             TICKERS.append(ticker)
 
         time.sleep(0.1)
 
 update()
-schedule.every(2).hours.do(update)
+schedule.every(6).hours.do(update)
 
 mean20 = 0
 ma5= 0
 def get_ma(ticker):
-    df = pyupbit.get_ohlcv(ticker, interval="minute1", count=25)
-    sum=0
-    for i in np.arange(0,20):
-        sum+=df['close'][i:5+i].rolling(5).mean().iloc[-1]  
+    df = pyupbit.get_ohlcv(ticker, interval="minute1", count=20)
+    ma5=df['close'].rolling(3).mean().iloc[-1]
+    ma20=df['close'].rolling(20).mean().iloc[-1]
 
-    mean20=sum/20
-    ma5=df['close'].rolling(5).mean().iloc[-1]
-
+    inclination = ma5 - df['close'][0:5].rolling(5).mean().iloc[-1]
+    
     # print(ticker)
     # print(mean20)
     # print(ma5)
 
-    return mean20 < ma5 < mean20*1.01
+    if inclination > 0 and ma5 >= ma20:
+        return 1 # Buy
+
+    if inclination < 0 and ma5 <= ma20:
+        return 0 # Sell
+
+    else:
+        return -1 # Nothing
 
 while True:
     schedule.run_pending()
     try:
         all = pyupbit.get_current_price(TICKERS)
         total = get_total()
-        start_time = get_start_time("KRW-BTC") + datetime.timedelta(minutes=30)
-        end_time = start_time + datetime.timedelta(days=1) - datetime.timedelta(minutes=1)
-        now = datetime.datetime.now()
 
-        if start_time < now < end_time:
-            for ticker in TICKERS:
-                krw = upbit.get_balance("KRW")
-                if get_ma(ticker):
-                    if krw > 5000 and upbit.get_balance(ticker) == 0:
-                        upbit.buy_market_order(ticker, total*0.197)
+        for ticker in TICKERS:
+            krw = upbit.get_balance("KRW")
+            bal = upbit.get_balance(ticker)
+            swch = get_ma(ticker)
+            
+            if swch == 1:
+                if krw > 5000 and bal == 0:
+                    upbit.buy_market_order(ticker, total*0.197)
 
-                elif upbit.get_balance(ticker) != 0 and (upbit.get_avg_buy_price(ticker) * 0.993 > all[ticker] or all[ticker] > upbit.get_avg_buy_price(ticker) * 1.002):
-                    upbit.sell_market_order(ticker, upbit.get_balance(ticker))
-                time.sleep(0.07)
-        else:
-            for ticker in TICKERS:
-                if upbit.get_balance(ticker) != 0:
-                    upbit.sell_market_order(ticker, upbit.get_balance(ticker))
-
-            update()
+            elif bal != 0 and swch == 0: 
+                upbit.sell_market_order(ticker, bal)
             
     except Exception as e:
         print(e)
